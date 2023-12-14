4fe7c8d42006a62a4ba4909c35c2f518352e732a
sriramulagopi <129825285+sriramulagopi@users.noreply.github.com>
2023-12-14 14:03:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function daysOfAyear(year){
+	if ((year%4==0 && year%100!=0)||(year%400==0)){
+		return 366;
+	}
+	else{
+		return 365;
+	}
+}
+let year = prompt("Enter the calendar year which year you want");
+console.log(daysOfAyear(2000));
